d67a22d55be46ed43bc34ea1014eb26b4e1e2504
kate7937 <56973685+kate7937@users.noreply.github.com>
2021-06-17 20:36:05 +0300

    Lab_2

---
+++ lab2/main.js
@@ -0,0 +1,49 @@
+var map;
+var marker;
+
+function initMap() {
+    marker = new google.maps.Marker();
+
+    const myLatLng = {lat: 49.22619321320788, lng: 28.41249926315556};
+
+    var infowindow = new google.maps.InfoWindow({});
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: myLatLng,
+    });
+
+    map.addListener('click', (e) => {
+        $.getJSON(
+            "https://api.openweathermap.org/data/2.5/weather?lat=" + e.latLng.lat() + "&lon=" + e.latLng.lng() + "&appid=1b5ee5a1a74d624a74750350327ea372",
+            function (json) {
+                var t = parseInt(json.main.temp - 273.15);
+                var feelsLike = parseInt(json.main.feels_like - 273.15);
+                console.log(json);
+                infowindow.setContent(
+                    "<div id='txt', class='display-6'>" +
+                    "<img src='http://openweathermap.org/img/wn/" + json.weather[0].icon + ".png' alt='альтернативный текст'>" +
+                    // "<h1>[thm</h1>"+
+                    "t = " +
+                    t.toString() +
+                    "&#8451\n" +
+                    "fl = " +
+                    feelsLike.toString() +
+                    "&#8451\n" +
+                    "</div>");
+                marker.setMap(null);
+                marker = new google.maps.Marker({
+                    position: e.latLng,
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 0
+                    }
+                });
+                infowindow.open(map, marker);
+
+            });
+
+    });
+
+}
